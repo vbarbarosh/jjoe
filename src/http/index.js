06dc89c556e1f79f3f417c74b0d85239fad2eebe
Vladimir Barbarosh <vladimir.barbarosh@gmail.com>
2021-12-22 00:12:43 +0200

    update /api/v1/diffs.json

--- src/http/index.js
+++ src/http/index.js
@@ -2,12 +2,9 @@ import cli from '@vbarbarosh/node-helpers/src/cli';
 import express from 'express';
 import express_routes from '@vbarbarosh/express-helpers/src/express_routes';
 import express_run from '@vbarbarosh/express-helpers/src/express_run';
-import fs_path_basename from '@vbarbarosh/node-helpers/src/fs_path_basename';
 import fs_path_resolve from '@vbarbarosh/node-helpers/src/fs_path_resolve';
-import fs_rm from '@vbarbarosh/node-helpers/src/fs_rm';
-import fs_write_stream from '@vbarbarosh/node-helpers/src/fs_write_stream';
 import knex from 'knex';
-import stream_promise from '@vbarbarosh/node-helpers/src/stream_promise';
+import array_index from '@vbarbarosh/node-helpers/src/array_index';
 
 const db = knex({client: 'mysql2', connection: 'mysql://jsonstory:jsonstory@127.0.0.1:3306/jsonstory'});
 const started = new Date();
@@ -42,12 +39,26 @@ async function items_list(req, res)
 
 async function diffs_list(req, res)
 {
-    res.json(await db('diffs').then(strip_ids));
+    res.json(await db('diffs').then(prep_diffs));
 }
 
 async function page404(req, res)
 {
     res.status(404).send(`Page not found: ${req.path}`);
+}
+
+async function prep_diffs(diffs)
+{
+    const items_map = array_index(await db('items').whereIn('id', diffs.map(v => v.item_id)), v => v.id);
+    const updates_map = array_index(await db('updates').whereIn('id', diffs.map(v => v.update_id)), v => v.id);
+    return JSON.parse(JSON.stringify(diffs)).map(function (diff) {
+        diff.item_uid = items_map[diff.item_id].uid;
+        diff.update_uid = updates_map[diff.update_id].uid;
+        delete diff.id;
+        delete diff.item_id;
+        delete diff.update_id;
+        return diff;
+    });
 }
 
 function strip_ids(items)
